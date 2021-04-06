793ec5ef0c26d72a7b16e246cda58dafa7ad0d2c
Vitamin_68 <wetalm68@gmail.com>
2021-04-07 01:48:58 +0300

    made Spring app with XML init made homework with 3 music style

--- src/main/java/ua/wetal/springcourse/TestSpring.java
+++ src/main/java/ua/wetal/springcourse/TestSpring.java
@@ -15,9 +15,11 @@ public class TestSpring {
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
 //        musicPlayer.playMusic();
+
         musicPlayer.playMusicList();
         System.out.println(musicPlayer.getName());
         System.out.println(musicPlayer.getVolume());
+
         context.close();
     }
 }
