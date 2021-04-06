b2a8cdbd9c8e09dcb70c23f3d29d39b8826edcac
Vitamin_68 <wetalm68@gmail.com>
2021-04-07 02:23:46 +0300

    - made Spring app with XML init - made homework with 3 music style -------- new branch "2_Life_cycle" - add 2 methods - init & destroy - use factory-metod for create Bean (made private constructor, add method in class & add factory-metod in XML

--- src/main/java/ua/wetal/springcourse/ClassicalMusic.java
+++ src/main/java/ua/wetal/springcourse/ClassicalMusic.java
@@ -1,6 +1,12 @@
 package ua.wetal.springcourse;
 
 public class ClassicalMusic implements Music {
+    private ClassicalMusic(){}
+
+    public static ClassicalMusic getClassicalMusic() {
+        return new ClassicalMusic();
+    }
+
     public void doMyInit() {
         System.out.println("Doing my initialization");
     }
