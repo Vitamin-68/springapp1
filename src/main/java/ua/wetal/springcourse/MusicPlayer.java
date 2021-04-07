76308c07ce72eaf9294361b029cf4aeaef7955ec
Vitamin_68 <wetalm68@gmail.com>
2021-04-08 02:29:51 +0300

    - made Spring app with XML init - made homework with 3 music style -------- new branch "2_Life_cycle" - add 2 methods - init & destroy - use factory-metod for create Bean (made private constructor, add method in class & add factory-metod in XML ------- new branch "3_Spring_Annotation" - use annotation in RockMusic & ClassicalMusic - changed xml & TestSpring ---- use @Autowired at field, constructor, setter ---- use @Qualifier

--- src/main/java/ua/wetal/springcourse/MusicPlayer.java
+++ src/main/java/ua/wetal/springcourse/MusicPlayer.java
@@ -1,19 +1,24 @@
 package ua.wetal.springcourse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
+//    @Autowired
+//    @Qualifier("classicalMusic")
+//    private Music music;
 
-    private ClassicalMusic classicalMusic;
-    private RockMusic rockMusic;
+    private Music music1;
+    private Music music2;
 
     @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
+    public MusicPlayer(@Qualifier("rockMusic") Music music1, @Qualifier("classicalMusic") Music music2) {
+        this.music1 = music1;
+        this.music2 = music2;
     }
+
 // 3. ID через поле
 //    @Autowired
 //    private Music music;
@@ -31,9 +36,11 @@ public class MusicPlayer {
 //    }
 
     public String playMusic() {
-//        System.out.println("Playing: " + music.getSong());
-        return "Playing: " + classicalMusic.getSong();
-//        System.out.println("Playing: " + classicalMusic.getSong());
-//        System.out.println("Playing: " + rockMusic.getSong());
+        return "Playing " + music1.getSong() + ", " + music2.getSong();
+//        return "Playing " + music.getSong();
+////        System.out.println("Playing: " + music.getSong());
+//        return "Playing: " + classicalMusic.getSong();
+////        System.out.println("Playing: " + classicalMusic.getSong());
+////        System.out.println("Playing: " + rockMusic.getSong());
     }
 }
